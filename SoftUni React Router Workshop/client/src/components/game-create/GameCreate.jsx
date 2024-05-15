cd4f35360a262e635dfab61bac14780b7565b379
Anton Todorov <antontodorov321@gmail.com>
2024-05-15 23:41:34 +0300

    Get all games service method

--- SoftUni React Router Workshop/client/src/components/game-create/GameCreate.jsx
+++ SoftUni React Router Workshop/client/src/components/game-create/GameCreate.jsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router-dom";
 
+import * as gameService from "../../services/gameService";
 
 export default function GameCreate() {
+    const navigate = useNavigate();
 
-    const createGameSubmitHandler = (e) => {
+    const createGameSubmitHandler = async (e) => {
         e.preventDefault();
 
         const gameData = Object.fromEntries(new FormData(e.currentTarget));
-
-        console.log(gameData);
+        try {
+            await gameService.create(gameData);
+            
+            navigate("/games");
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
